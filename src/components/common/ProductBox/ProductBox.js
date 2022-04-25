dd93eda3e777eadc559a16302e2864edc25c1a02
Dmitry Skok <dimas.skok95@gmail.com>
2022-04-25 21:57:11 +0200

    Fix ProductBox icon FaHeart import

--- src/components/common/ProductBox/ProductBox.js
+++ src/components/common/ProductBox/ProductBox.js
@@ -1,28 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import styles from './ProductBox.module.scss';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHeart, faStar, faExchangeAlt, faShoppingBasket} from '@fortawesome/free-solid-svg-icons';
-import {faStar as farStar, faHeart} from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faStar,
+  faExchangeAlt,
+  faShoppingBasket,
+} from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar, faHeart } from '@fortawesome/free-regular-svg-icons';
 import Button from '../Button/Button';
-import {toggleProductFavorite} from '../../../redux/productsRedux';
+import { toggleProductFavorite } from '../../../redux/productsRedux';
 
-const ProductBox = ({name, price, oldPrice, promo, stars, isFavorite, toCompare, image, id}) => {
+const ProductBox = ({
+  name,
+  price,
+  oldPrice,
+  promo,
+  stars,
+  isFavorite,
+  toCompare,
+  image,
+  id,
+}) => {
   const dispatch = useDispatch();
-  const handleClickFavorite = (e) => {
+  const handleClickFavorite = e => {
     e.preventDefault();
     dispatch(toggleProductFavorite(id));
   };
 
   return (
     <div className={styles.root}>
-      <div className={styles.photo} style={{backgroundImage: `url(${image})`}}>
+      <div className={styles.photo} style={{ backgroundImage: `url(${image})` }}>
         {promo && <div className={styles.sale}>{promo}</div>}
         <div className={styles.buttons}>
-          <Button variant="small">Quick View</Button>
-          <Button variant="small">
+          <Button variant='small'>Quick View</Button>
+          <Button variant='small'>
             <FontAwesomeIcon icon={faShoppingBasket}></FontAwesomeIcon> ADD TO CART
           </Button>
         </div>
@@ -30,9 +44,13 @@ const ProductBox = ({name, price, oldPrice, promo, stars, isFavorite, toCompare,
       <div className={styles.content}>
         <h5>{name}</h5>
         <div className={styles.stars}>
-          {[1, 2, 3, 4, 5].map((i) => (
-            <a key={i} href="#">
-              {i <= stars ? <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon> : <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>}
+          {[1, 2, 3, 4, 5].map(i => (
+            <a key={i} href='#'>
+              {i <= stars ? (
+                <FontAwesomeIcon icon={faStar}>{i} stars</FontAwesomeIcon>
+              ) : (
+                <FontAwesomeIcon icon={farStar}>{i} stars</FontAwesomeIcon>
+              )}
             </a>
           ))}
         </div>
@@ -40,16 +58,20 @@ const ProductBox = ({name, price, oldPrice, promo, stars, isFavorite, toCompare,
       <div className={styles.line}></div>
       <div className={styles.actions}>
         <div className={styles.outlines}>
-          <Button variant="outline" isFavorite={isFavorite} onClick={handleClickFavorite}>
+          <Button
+            variant='outline'
+            isFavorite={isFavorite}
+            onClick={handleClickFavorite}
+          >
             <FontAwesomeIcon icon={faHeart}>Favorite</FontAwesomeIcon>
           </Button>
-          <Button variant="outline" toCompare={toCompare}>
+          <Button variant='outline' toCompare={toCompare}>
             <FontAwesomeIcon icon={faExchangeAlt}>Add to compare</FontAwesomeIcon>
           </Button>
         </div>
         <div className={styles.price}>
           <div className={styles.oldPrice}>{oldPrice}</div>
-          <Button noHover variant="small">
+          <Button noHover variant='small'>
             $ {price}
           </Button>
         </div>
